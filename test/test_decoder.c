1c19ffde1fab536ae41fe5defe9c67b8a7f60d18
KubaTaba1uga <luna7337anul@proton.me>
2025-05-20 06:15:40 +0200

    add missing license banners

--- test/test_decoder.c
+++ test/test_decoder.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025 Jakub Buczynski <KubaTaba1uga>
+ * SPDX-License-Identifier: MIT
+ * See LICENSE file in the project root for full license information.
+ */
+
 #include <stdlib.h>
 #include <string.h>
 #include <unity.h>
